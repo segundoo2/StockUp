4c4d142f8bda764bd825794cf83a325c5314ed6b
Edilson Segundo <edilsonsilva13@gmail.com>
2026-06-08 18:31:43 -0300

    feat - implementa class validator e swagger no dto create-users

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,34 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  //configuração do class-validator/class-transform
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
+  // Configuração do Swagger
+  const config = new DocumentBuilder()
+    .setTitle('StockUp API')
+    .setDescription(
+      'Documentação da API do StockUp, um sistema de gestão e controle de estoque.',
+    )
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
-bootstrap();
+void bootstrap();
---
+++ backend/src/modules/users/enum/errors.enum.ts
@@ -0,0 +1,4 @@
+export enum EErrors {
+  USERNAME_LENGTH = 'O username deve ter entre 3 e 50 caracteres.',
+  USERNAME_CHARACTERS = 'O username deve conter apenas letras minúsculas, números e ponto final.',
+}

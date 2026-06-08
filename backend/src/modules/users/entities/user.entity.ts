20e09bb1a454297e2c3acf91e312211ed4664000
Edilson Segundo <edilsonsilva13@gmail.com>
2026-06-08 17:47:58 -0300

    feat - implementa entidade users

---
+++ backend/src/modules/users/entities/user.entity.ts
@@ -0,0 +1,25 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity({ name: 'users' })
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id!: string;
+
+  @Column({ type: 'varchar', unique: true, length: 50 })
+  username!: string;
+
+  @Column({ type: 'varchar', length: 20 })
+  password!: string;
+
+  @CreateDateColumn()
+  createAt!: Date;
+
+  @UpdateDateColumn()
+  updateAt!: Date;
+}

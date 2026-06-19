bd850a153a99d70faaf86145a10924f0b9ae43f3
Edilson Segundo <edilsonsilva13@gmail.com>
2026-06-18 22:58:38 -0300

    feat - add o UsersRepository nos providers do UsersModule

--- backend/src/modules/users/users.module.ts
+++ backend/src/modules/users/users.module.ts
@@ -3,10 +3,14 @@ import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
+import { UsersRepository } from './users.repository';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
   controllers: [UsersController],
-  providers: [{ provide: 'IUsersService', useClass: UsersService }],
+  providers: [
+    { provide: 'IUsersService', useClass: UsersService },
+    { provide: 'IUsersRepository', useClass: UsersRepository },
+  ],
 })
 export class UsersModule {}

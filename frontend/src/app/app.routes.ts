5d9bc8dd7b9a232a2161d13dc2e446e8925f578c
Edilson Segundo <edilsonsilvaa13@gmail.com>
2026-09-08 17:30:28 -0300

    chore - configura tailwind e organiza a estrutura de pastas. feat - inicia implementação do componente auth

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full',
+  },
+  {
+    path: 'login',
+    loadComponent: () => import('./features/auth/login.component').then((m) => m.LoginComponent),
+  },
+  {
+    path: '**',
+    redirectTo: 'login',
+  },
+];
---
+++ frontend/src/app/features/auth/login.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-login.component',
+  styles: ``,
+  templateUrl: './login.component.html',
+})
+export class LoginComponent {}
